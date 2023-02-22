edc42f04a2daaad0fa35c6ce67f3325f0d19877a
AliceDell <alicedelluntoo@gmail.com>
2023-02-22 15:59:02 +0100

    mise à jour du code css + js

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,32 +1,36 @@
-function createEl(element) {
-    return document.createElement(element);
-}
-
-function parentAppend(parent, el) {
-  return parent.appendChild(el);
-}
-
 const galery = document.getElementById("gallery");
 const url = 'http://localhost:5678/api/works';
 
 fetch(url)
 .then((resp) => resp.json())
-.then(function(data) {
-  let work = data.results;
-  return work.map(function(project) {
-    console.log(project);
-    console.log("tst");
-   
-    /*let images = document.createElement("img")
-    image.src = projets.imageUrl;
-    image.alt = projets.title;
+.then(function(data) { 
+  let work = data;
+    return work.map(function(work) {
+    console.log(work);
     
+   
+    let images = document.createElement("img")
+    images.src = work.imageUrl;
+    images.alt = work.title;
+    images.crossorigin="anonymous";
+
     let fig = document.createElement("figurecaption");
     fig.innerHTML = work.title;
     
     let figure = document.createElement("figure");
-    figure.appendchild(images)
-    figure.appendchild(fig)
-    */
+    figure.appendChild(images);
+    figure.appendChild(fig);
+    galery.appendChild(figure);
+  // a faire :   
+ // déclarer un tableau var récupérer tout les projet par categoryid var work /
+ // function de mettre à jour 
+ // function qui récupére de tout les works et recherche un projet un va être ajouter dans un nouveau tableau
+ // crée une fonction qui met à jour + la recherche
+
+  // récupérer tout les projets
+    var getAllWorks = work;
+  // catégories
+  // recherche de catégorie
+  // mettre à jour  
   })
 })
