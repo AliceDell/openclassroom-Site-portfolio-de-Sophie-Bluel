cdde74adcf2e50d1223f7d7bc1e896fac63aad5c
AliceDell <alicedelluntoo@gmail.com>
2023-02-28 18:38:09 +0100

    mise à jour boutton

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,9 +1,19 @@
 const galery = document.getElementById("gallery");
 const url = 'http://localhost:5678/api/works';
 
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data) { 
+
+ // a faire :   
+ // déclarer un tableau var récupérer tout les projet par categoryid var work /
+ // function de mettre à jour 
+ // function qui récupére de tout les works et recherche un projet un va être ajouter dans un nouveau tableau
+ // crée une fonction qui met à jour + la recherche
+
+  // récupérer tout les projets
+  var getAllWorks = document.getElementById("button-tous");
+  getAllWorks.addEventListener("click",() => {
+    fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) { 
   let work = data;
     return work.map(function(work) {
     console.log(work);
@@ -21,25 +31,22 @@ fetch(url)
     figure.appendChild(images);
     figure.appendChild(fig);
     galery.appendChild(figure);
-  // a faire :   
- // déclarer un tableau var récupérer tout les projet par categoryid var work /
- // function de mettre à jour 
- // function qui récupére de tout les works et recherche un projet un va être ajouter dans un nouveau tableau
- // crée une fonction qui met à jour + la recherche
+})
 
-  // récupérer tout les projets
-    var getAllWorks = document.getElementsByClassName(".button-tous");
-      getAllWorks.addEventListener("click",() => {
-        const text = "j'ai cliqué sur le bouton tous";
-        getAllWorks.innerHTML = text;
-      });
+  });
+    });
 
-  // catégories
-
-});
 
   // recherche de catégorie
+var searchcat = document.getElementById("button-objets");
+getAllWorks.addEventListener("click",() => {
+  console.log(searchcat);
+  return work.map(function(work) {
+      console.log(work);
+    
+// faire un tableau avec array en faisant une fonction qui retourne les élements
       //gestion des bouttons 
       
   // mettre à jour  
-  })
+  });
+});
